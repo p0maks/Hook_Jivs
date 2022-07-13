d971d9bda838906dcd07b0dd2202cec8662cd13f
p0maks <golosova_v19@mail.ru>
2022-07-13 20:18:24 +0300

    one commit

--- jivs_on_python/jivs_on_python.py
+++ jivs_on_python/jivs_on_python.py
@@ -1,9 +1,7 @@
 def Function(Point):
     x1 = Point[0]
     x2 = Point[1]
-    func = (1 - x1)**2 + 100*(x2 - x1**2)**2   # розенброка (1, 1) = 0
-    #func = x1**2*x2**2 + 2     # (0, 0) ~ 2
-    #func = (x1 + 2*x2 - 7)**2 + (2*x1 + x2 - 5)**2   # бута (1, 3) = 0
+    func = (1 - x1)**2 + 100*(x2 - x1**2)**2   
     return func
 
 
@@ -29,50 +27,3 @@ def expSearch(Point, delta):
         else:
             newPoint.append(Point[1])
     return newPoint
-
-
-def HookeJeeves(startPoint, delta, accuracy):
-    print('Начальная точка:', startPoint[0], startPoint[1], '; F(x, y) =', Function(startPoint))
-    stop = False
-    Point1 = startPoint
-    while not stop:
-        print()
-        print('     Исслед. поиск:')
-        delta_ = delta
-        while not stop and Point1 == startPoint:
-            Point1 = expSearch(startPoint, delta_)  
-            if delta_ > accuracy:
-                delta_ = delta_/10
-            else:
-                stop = True
-            print('нашел точку:', Point1[0], Point1[1], Function(Point1))
-        print('     Результат поиска:', Point1[0], Point1[1], Function(Point1))
-        print()
-        print('     Поиск по образцу:')
-
-        Point2 = Point1
-        isLess = True
-        while not stop and isLess:
-            Point1 = Point2
-            x1 = startPoint[0] + 2*(Point1[0] - startPoint[0])
-            x2 = startPoint[1] + 2*(Point1[1] - startPoint[1])
-            Point2 = expSearch([x1, x2], delta)
-            if Function(Point1) <= Function(Point2):
-                isLess = False
-            print('нашел точку:', Point2[0], Point2[1], Function(Point2))
-        print('     Результат поиска:', Point1[0], Point1[1], Function(Point1))
-        startPoint = Point1
-
-    result = startPoint
-    return result
-
-
-x1 = 1.5
-x2 = 2.0
-Point = [x1, x2]
-delta = 0.0004
-accuracy = 0.00001
-result = HookeJeeves(Point, delta, accuracy)
-print()
-print('Результат работы алгоритма:', result[0], result[1], Function(result))
-print()
